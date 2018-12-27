933036353ff62a1486ea5d5abff874daf664281e
ManlyMarco <dummyaccx@gmail.com>
2018-12-27 14:19:55 +0100

    Better setting descriptions; Version up to 1.2

--- src/FolderBrowser/FolderBrowser.cs
+++ src/FolderBrowser/FolderBrowser.cs
@@ -8,7 +8,7 @@ namespace BrowserFolders
     public class KK_BrowserFolders : BaseUnityPlugin
     {
         public const string Guid = "marco.FolderBrowser";
-        public const string Version = "1.1";
+        public const string Version = "1.2";
 
         private SceneFolders _sceneFolders;
         private MakerFolders _makerFolders;
@@ -16,19 +16,29 @@ namespace BrowserFolders
         private FreeHFolders _freeHFolders;
 
         [DisplayName("Enable folder browser in maker")]
+        [Category("Main game")]
         [Description("Changes take effect on game restart")]
         public static ConfigWrapper<bool> EnableMaker { get; private set; }
-        [DisplayName("Enable folder browser in classroom browser")]
+        [DisplayName("Enable folder browser in classroom/new game browser")]
+        [Category("Main game")]
         [Description("Changes take effect on game restart")]
         public static ConfigWrapper<bool> EnableClassroom { get; private set; }
+
         [DisplayName("Enable folder browser in Free H browser")]
+        [Category("Main game")]
         [Description("Changes take effect on game restart")]
         public static ConfigWrapper<bool> EnableFreeH { get; private set; }
-        [DisplayName("Enable folder browser in studio")]
+
+        [DisplayName("Enable folder browser in scene browser")]
+        [Category("Chara Studio")]
         [Description("Changes take effect on game restart")]
         public static ConfigWrapper<bool> EnableStudio { get; private set; }
+
         [DisplayName("Save scenes to current folder")]
-        [Description("When you select a custom folder to load a scene from, newly saved scenes will be saved to this folder.\nIf disabled, scenes are always saved to default folder (studio/scene).")]
+        [Category("Chara Studio")]
+        [Description("When you select a custom folder to load a scene from, " +
+                     "newly saved scenes will be saved to this folder.\n" +
+                     "If disabled, scenes are always saved to default folder (studio/scene).")]
         public static ConfigWrapper<bool> StudioSaveOverride { get; private set; }
 
         private void OnGUI()
