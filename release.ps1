c5f5f8e351187da6a60fbaf7a643c6ce2c980493
ManlyMarco <dummyaccx@gmail.com>
2020-06-09 19:07:22 +0200

    Update release script to add hs2 and retry on fail

--- release.ps1
+++ release.ps1
@@ -5,25 +5,35 @@ else {
     $dir = $PSScriptRoot + "\bin\"
 }
 
-$array = @("KK", "AI")
-
+$array = @("KK", "AI", "HS2")
 
 $copy = $dir + "\copy\BepInEx" 
 
-
 New-Item -ItemType Directory -Force -Path ($dir + "\out")  
 
-foreach ($element in $array) {
+function CreateZip ($element)
+{
+    Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
+    New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
 
-Remove-Item -Force -Path ($dir + "\copy") -Recurse -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Force -Path ($copy + "\plugins")
+    Copy-Item -Path ($dir + "\BepInEx\plugins\" + $element + "*.*") -Destination ($copy + "\plugins\" ) -Recurse -Force 
 
-Copy-Item -Path ($dir + "\BepInEx\plugins\" + $element + "*.*") -Destination ($copy + "\plugins\" ) -Recurse -Force 
+    $ver = "v" + (Get-ChildItem -Path ($copy) -Filter "*.dll" -Recurse -Force)[0].VersionInfo.FileVersion.ToString()
 
-$ver = "v" + (Get-ChildItem -Path ($copy) -Filter "*.dll" -Recurse -Force)[0].VersionInfo.FileVersion.ToString()
+    Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + $element + "_BrowserFolders_" + $ver + ".zip")
+}
 
-Compress-Archive -Path $copy -Force -CompressionLevel "Optimal" -DestinationPath ($dir + "out\" + $element + "_BrowserFolders_" + $ver + ".zip")
-
+foreach ($element in $array) 
+{
+    try
+    {
+        CreateZip ($element)
+    }
+    catch 
+    {
+        # retry
+        CreateZip ($element)
+    }
 }
 
 Remove-Item -Force -Path ($dir + "\copy") -Recurse
