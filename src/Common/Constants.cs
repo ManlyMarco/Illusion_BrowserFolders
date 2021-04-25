43665750a8669ea3506b30f2867855cb91afaaa5
ManlyMarco <dummyaccx@gmail.com>
2021-04-25 21:23:29 +0200

    Version up to 2.5

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -3,6 +3,6 @@
     internal static class Constants
     {
         public const string Guid = "marco.FolderBrowser";
-        public const string Version = "2.4.1";
+        public const string Version = "2.5";
     }
 }
