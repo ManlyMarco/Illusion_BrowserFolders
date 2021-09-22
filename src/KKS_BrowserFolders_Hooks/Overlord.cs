5ae73b3f5ee36d0480098e4f3741d6976e012371
Sai <boulton123@gmail.com>
2021-09-22 22:37:01 +0100

    Converts cards in subdirectories on game boot (+1 squashed commits)
    Squashed commits:
    
    [a866c1a] Converts cards in subdirectories on game boot

--- src/KKS_BrowserFolders_Hooks/Overlord.cs
+++ src/KKS_BrowserFolders_Hooks/Overlord.cs
@@ -20,6 +20,61 @@ namespace BrowserFolders.Hooks.KKP
             Harmony.CreateAndPatchAll(typeof(Overlord));
         }
 
+        //In case of duplicate names in different folders
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(File), nameof(File.Move), typeof(string), typeof(string))]
+        internal static void MoveHook(string sourceFileName, string destFileName)
+        {
+            if(File.Exists(destFileName))
+                File.Delete(destFileName);
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ConvertChaFileScene), "Start")]
+        internal static void ConvertStartHookPre(ref ConvertChaFileScene __instance)
+        {
+            var sexList = __instance.convKoikatsuCha;
+            for (int i = 0; i < sexList.Length; i++)
+            {
+                string dir = "UserData/chara";
+                if (i == 1)
+                    dir = Path.Combine(dir, "female");
+                else
+                    dir = Path.Combine(dir, "male");
+
+                GetDirs(dir, ref __instance, i);
+            }
+
+        }
+
+        private static void GetDirs(string dir, ref ConvertChaFileScene instance, int sex)
+        {
+            if (!dir.Equals("UserData/chara\\female\\_autosave") && !dir.Equals("UserData/chara\\male\\_autosave"))
+            {
+                foreach (var folder in Directory.GetDirectories(dir))
+                {
+                    GetDirs(folder, ref instance, sex);
+                }
+
+                if(!dir.Equals("UserData/chara\\female") && !dir.Equals("UserData/chara\\male"))
+                    AddFilesToConvList(dir, ref instance, sex);
+            }
+        }
+
+        private static void AddFilesToConvList(string dir, ref ConvertChaFileScene instance, int sex)
+        {
+            foreach (var file in Directory.GetFiles(dir))
+            {
+                using (var reader = File.Open(file, FileMode.Open))
+                {
+                    var charFile = new ChaFile();
+                    if(charFile.LoadFileKoikatsu(new BinaryReader(reader), false, true))
+                        instance.convKoikatsuCha[sex].Add(new ConvertChaFileScene.PackData(file, 1)); //Mode must be 1
+                }
+            }
+        }
+
+
         //FreeH caches male and female causing window to be stuck on the most last opened sex. Forcing recycle to be false fixes this
         [HarmonyPrefix]
         [HarmonyPatch(typeof(FreeHCharaSelect), nameof(FreeHCharaSelect.Create), typeof(int), typeof(bool))]
