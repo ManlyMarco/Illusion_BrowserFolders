38c72f805783ba399d9af3ee7f62127768499af5
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-11-03 17:46:50 +0100

    Version up to 3.1

--- src/Common/Constants.cs
+++ src/Common/Constants.cs
@@ -3,6 +3,6 @@
     internal static class Constants
     {
         public const string Guid = "marco.FolderBrowser";
-        public const string Version = "3.0";
+        public const string Version = "3.1";
     }
 }
